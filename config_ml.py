38e48b75f6c3af48cb5e780a09b9a6e9da94042b
Renato Faria <renato.faria6@icloud.com>
2026-06-20 18:22:44 -0300

    Adicionar configuracao isolada Mercado Livre

---
+++ config_ml.py
@@ -0,0 +1,8 @@
+ATIVAR_MERCADO_LIVRE = False
+
+def mercado_livre_ativo():
+    return ATIVAR_MERCADO_LIVRE
+
+if __name__ == "__main__":
+    print("ATIVAR_MERCADO_LIVRE:", ATIVAR_MERCADO_LIVRE)
+    print("mercado_livre_ativo():", mercado_livre_ativo())
